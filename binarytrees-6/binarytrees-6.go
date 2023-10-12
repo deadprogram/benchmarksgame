c0a48c210b416192760d9e08dc5629262a638a5f
deadprogram <ron@hybridgroup.com>
2023-10-12 12:51:37 +0200

    all: go fmt code
    Signed-off-by: deadprogram <ron@hybridgroup.com>

--- binarytrees-6/binarytrees-6.go
+++ binarytrees-6/binarytrees-6.go
@@ -8,94 +8,92 @@
 package main
 
 import (
-   "flag"
-   "fmt"
-   "strconv"
-   "sync"
+	"flag"
+	"fmt"
+	"strconv"
+	"sync"
 )
 
 type Node struct {
-   left, right *Node
+	left, right *Node
 }
 
-var pool = sync.Pool {
-     New: func() interface{} {
-          return &Node{}
-     },
+var pool = sync.Pool{
+	New: func() interface{} {
+		return &Node{}
+	},
 }
 
 const minDepth = 4
 
 func trees(maxDepth int) {
-   longLastingNode := createTree(maxDepth)
-   depth := 4
+	longLastingNode := createTree(maxDepth)
+	depth := 4
 
-   for depth <= maxDepth {
-      iterations := 1 << uint(maxDepth-depth+minDepth) // 16 << (maxDepth - depth)
+	for depth <= maxDepth {
+		iterations := 1 << uint(maxDepth-depth+minDepth) // 16 << (maxDepth - depth)
 
-      loops(iterations, depth)
-      depth += 2
-   }
-   fmt.Printf("long lived tree of depth %d\t check: %d\n", maxDepth,
-      checkTree(longLastingNode))
+		loops(iterations, depth)
+		depth += 2
+	}
+	fmt.Printf("long lived tree of depth %d\t check: %d\n", maxDepth,
+		checkTree(longLastingNode))
 }
 
 func loops(iterations, depth int) {
-   check := 0
-   item := 0
-   for item < iterations {
-      t := createTree(depth)
-      check += checkTree(t)
-      pool.Put(t)
-      item++
-   }
-   fmt.Printf("%d\t trees of depth %d\t check: %d\n",
-      iterations, depth, check)
+	check := 0
+	item := 0
+	for item < iterations {
+		t := createTree(depth)
+		check += checkTree(t)
+		pool.Put(t)
+		item++
+	}
+	fmt.Printf("%d\t trees of depth %d\t check: %d\n",
+		iterations, depth, check)
 }
 
 func checkTree(n *Node) int {
-   if n.left == nil {
-      // parent will sync.Pool.Put
-      return 1
-   }
-   check := checkTree(n.left) + checkTree(n.right) + 1
-   pool.Put(n.left)
-   n.left = nil
-   pool.Put(n.right)
-   n.right = nil
-   return check
+	if n.left == nil {
+		// parent will sync.Pool.Put
+		return 1
+	}
+	check := checkTree(n.left) + checkTree(n.right) + 1
+	pool.Put(n.left)
+	n.left = nil
+	pool.Put(n.right)
+	n.right = nil
+	return check
 }
 
 func createTree(depth int) *Node {
-   node := pool.Get().(*Node)
-   if depth > 0 {
-      depth--
-      node.left = createTree(depth)
-      node.right = createTree(depth)
-   }
-   return node
+	node := pool.Get().(*Node)
+	if depth > 0 {
+		depth--
+		node.left = createTree(depth)
+		node.right = createTree(depth)
+	}
+	return node
 }
-
 
 func main() {
-   n := 0
-   flag.Parse()
-   if flag.NArg() > 0 {
-      n, _ = strconv.Atoi(flag.Arg(0))
-   }
+	n := 0
+	flag.Parse()
+	if flag.NArg() > 0 {
+		n, _ = strconv.Atoi(flag.Arg(0))
+	}
 
-   maxDepth := n
-   if minDepth+2 > n {
-      maxDepth = minDepth + 2
-   }
+	maxDepth := n
+	if minDepth+2 > n {
+		maxDepth = minDepth + 2
+	}
 
-   {
-      stretchDepth := maxDepth + 1
-      t := createTree(stretchDepth)
-      check := checkTree(t)
-      pool.Put(t)
-      fmt.Printf("stretch tree of depth %d\t check: %d\n", stretchDepth, check)
-   }
-   trees(maxDepth)
+	{
+		stretchDepth := maxDepth + 1
+		t := createTree(stretchDepth)
+		check := checkTree(t)
+		pool.Put(t)
+		fmt.Printf("stretch tree of depth %d\t check: %d\n", stretchDepth, check)
+	}
+	trees(maxDepth)
 }
-
